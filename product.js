c778b3001f1857d20d262cd62ca22b7bc8ea954c
hshehabu <hamzashehabu457@gmail.com>
2023-06-02 23:45:10 +0300

    input group and group text done

--- product.js
+++ product.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
+$(document).ready(
+  function () {
   $(".select2").select2();
+
+  function changeDateTime() {
+    var dateTimeInput = document.getElementById("myDateTimeInput");
+    dateTimeInput.value = "2023-06-02T12:34"; // Set the new value here
+  }
+  
 });
